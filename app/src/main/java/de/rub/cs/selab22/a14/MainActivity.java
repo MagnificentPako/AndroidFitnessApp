5e740b4a0911ae45194f554867964d253e6fe879
BatoulAbiZaid <95659955+BatoulAbiZaid@users.noreply.github.com>
2022-07-01 20:21:35 +0200

    MoodFeelings update

--- app/src/main/java/de/rub/cs/selab22/a14/MainActivity.java
+++ app/src/main/java/de/rub/cs/selab22/a14/MainActivity.java
@@ -2,8 +2,10 @@ package de.rub.cs.selab22.a14;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import de.rub.cs.selab22.a14.mood.MoodFeelings;
 import de.rub.cs.selab22.a14.settings.I18nAppCompatActivity;
 import de.rub.cs.selab22.a14.settings.SettingsActivity;
 
@@ -16,10 +18,19 @@ public class MainActivity extends I18nAppCompatActivity {
 
         Button settingsButton = findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(view -> goToSettings());
+        // mood
+        Button moodButton = findViewById(R.id.mood_button);
+        moodButton.setOnClickListener(view -> goToMoodActivity());
     }
 
     private void goToSettings() {
         Intent switchActivity = new Intent(this, SettingsActivity.class);
+        startActivity(switchActivity);
+    }
+
+
+    private void goToMoodActivity() {
+        Intent switchActivity = new Intent(this, MoodFeelings.class);
         startActivity(switchActivity);
     }
 }
