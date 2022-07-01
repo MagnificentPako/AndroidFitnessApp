fb1e943f6c6a68d76f95bb30a1ded8a2e0d6267b
BatoulAbiZaid <95659955+BatoulAbiZaid@users.noreply.github.com>
2022-07-01 23:30:39 +0200

    MoodActivity update

--- app/src/main/java/de/rub/cs/selab22/a14/mood/LastPage.java
+++ app/src/main/java/de/rub/cs/selab22/a14/mood/LastPage.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import de.rub.cs.selab22.a14.MainActivity;
 import de.rub.cs.selab22.a14.R;
 
 public class LastPage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_page);
+
+        Button doneButton = findViewById(R.id.done_button);
+        doneButton.setOnClickListener(view -> goToMainPage());
     }
-    public void done_button(View view){
-        Intent intent = new Intent(LastPage.this, MoodActivity.class);
+    public void goToMainPage(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/de/rub/cs/selab22/a14/mood/MoodActivity.java
+++ app/src/main/java/de/rub/cs/selab22/a14/mood/MoodActivity.java
@@ -2,7 +2,9 @@ package de.rub.cs.selab22.a14.mood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import de.rub.cs.selab22.a14.R;
 
@@ -12,5 +14,14 @@ public class MoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
+    }
+    public void mood_record(View view) {
+        Intent intent = new Intent(MoodActivity.this, MoodFeelings.class);
+        startActivity(intent);
+    }
+
+    public void notification_button(View view) {
+        Intent intent = new Intent(MoodActivity.this, Notification.class);
+        startActivity(intent);
     }
 }
