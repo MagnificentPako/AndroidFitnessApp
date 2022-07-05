9afcd7312be808dc29ff4eea5db4b3a20de502f8
Cankoek <can.koeksen@rub.de>
2022-07-06 00:42:43 +0200

    Add graph to mood

--- app/src/main/java/de/rub/cs/selab22/a14/mood/MoodActivity.java
+++ app/src/main/java/de/rub/cs/selab22/a14/mood/MoodActivity.java
@@ -5,16 +5,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import de.rub.cs.selab22.a14.R;
 
 public class MoodActivity extends AppCompatActivity {
 
+    LineChart lc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
+
+        Button dayButton = findViewById(R.id.mood_day_button);
+        Button weekButton = findViewById(R.id.mood_week_button);
+        Button monthButton = findViewById(R.id.mood_month_button);
+
+        lc = (LineChart) findViewById(R.id.mood_chart);
+
+        setStyleConfig();
+        renderActivity("Weekly", 7 );
+
+        dayButton.setOnClickListener(onClickListener);
+        weekButton.setOnClickListener(onClickListener);
+        monthButton.setOnClickListener(onClickListener);
     }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(final View v) {
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            switch(v.getId()){
+                case R.id.mood_day_button:
+                    renderActivity("Daily", 3 );
+                    break;
+                case R.id.mood_week_button:
+                    renderActivity("Weekly", 7 );
+                    break;
+                case R.id.mood_month_button:
+                    renderActivity("Monthly", 4 );
+                    break;
+            }
+
+        }
+    };
+
     public void mood_record(View view) {
         Intent intent = new Intent(MoodActivity.this, MoodFeelings.class);
         startActivity(intent);
@@ -23,5 +78,85 @@ public class MoodActivity extends AppCompatActivity {
     public void notification_button(View view) {
         Intent intent = new Intent(MoodActivity.this, Notification.class);
         startActivity(intent);
+    }
+
+    private void setStyleConfig()
+    {
+        lc.setDescription(descriptionHandler("Mood", 15f, true));
+        lc.setDrawBorders(true);
+        lc.setBorderWidth(1f);
+        axisHandler();
+    }
+
+    private void axisHandler ()
+    {
+        XAxis xAxis = lc.getXAxis();
+        xAxis.setAxisMinimum(1);
+
+        YAxis yAxisLeft = lc.getAxisLeft();
+        YAxis yAxisRight = lc.getAxisRight();
+
+        yAxisRight.setAxisMinimum(0);
+        yAxisRight.setAxisMaximum(100);
+        yAxisLeft.setAxisMinimum(0);
+        yAxisLeft.setAxisMaximum(100);
+
+        xAxis.setDrawGridLines(false);
+        yAxisRight.setDrawGridLines(false);
+        yAxisLeft.setDrawGridLines(false);
+        xAxis.setGranularity(1f);
+        yAxisRight.setGranularity(1f);
+        yAxisLeft.setGranularity(1f);
+
+        xAxis.setDrawAxisLine(false);
+    }
+
+    private Description descriptionHandler (String description, float textSize, Boolean active)
+    {
+        Description des = lc.getDescription();
+        des.setTextSize(textSize);
+        des.setText(description);
+        if (active)
+        {
+            des.setEnabled(true);
+        } else {
+            des.setEnabled(false);
+        }
+        return des;
+    }
+
+    private void renderActivity(String label, int length) {
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        ArrayList<Entry> entryList = createEntryList(length);
+
+        //ArrayList<Entry> entryList2 = createEntryList(length);
+        //LineDataSet lineDataSet2 = new LineDataSet(entryList2, "Test");
+        //lineDataSet2.setColor(0xff00ff00);
+        //dataSets.add(lineDataSet2);
+
+        LineDataSet lineDataSet = new LineDataSet(entryList, label);
+        dataSets.add(lineDataSet);
+
+        LineData data = new LineData(dataSets);
+        lc.setData(data);
+
+        setStyleConfig();
+        lc.invalidate();
+    }
+
+    private ArrayList<Entry> createEntryList (int length)
+    {
+        ArrayList<Entry> dataVals = new ArrayList<>();
+        for (int i = 0; i < length; i++)
+        {
+            //DB Data to be inserted here instead of randomInt
+            Random ran = new Random();
+            int randomInt = ran.nextInt(100) + 0;
+
+            dataVals.add(new Entry(i+1, randomInt));
+        }
+
+
+        return dataVals;
     }
 }
