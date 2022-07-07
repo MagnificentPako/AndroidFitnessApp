957eea32491f04bbd8c4ee869d5348579f1f0ea3
Paul Kosel <my@procrastination.life>
2022-07-07 15:52:36 +0200

    can wollte, dass ich pushe

---
+++ app/src/main/java/de/rub/cs/selab22/a14/navigation/ActivityFragment.java
@@ -0,0 +1,13 @@
+package de.rub.cs.selab22.a14.navigation;
+
+import androidx.fragment.app.Fragment;
+
+import de.rub.cs.selab22.a14.R;
+
+public class ActivityFragment extends Fragment {
+
+    public ActivityFragment() {
+        super(R.layout.activity_fragment);
+    }
+
+}
